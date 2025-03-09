2969a37ac322d58a3f01cd3d37f4672bab529f50
turbo <turbobigsnail@mail.ru>
2025-03-09 17:35:20 +0300

    less-1

--- lesson_2/lesson_2.py
+++ lesson_2/lesson_2.py
@@ -4,7 +4,10 @@ pepper_dust = int(input("введите кол-во 'огненной пыли':
 moonshine = int(input("введите кол-во 'лунного света': "))
 water = int(input("введите кол-во воды: "))
 
-total = sum([herbs * 0.5, cristals * 1.5, pepper_dust * 1.0, moonshine * 1.2, water * 0.8])
-bonus = total + ((total > 150) * 20)
+sum_ingridients =  sum([herbs, cristals, pepper_dust, moonshine, water])
+bonus = ((sum_ingridients > 150) * 20)
+# print(bonus)
 
-print("Магическая сила зелья:", bonus)
+total = sum([herbs * 0.5, cristals * 1.5, pepper_dust * 1.0, moonshine * 1.2, water * 0.8]) + bonus
+
+print("Магическая сила зелья:", total)
