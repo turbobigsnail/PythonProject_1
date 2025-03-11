9fa981d56d7a67bc17587a9af89a093394d26c48
turbo <turbobigsnail@mail.ru>
2025-03-11 20:55:20 +0300

    another_commit

--- lesson_3/lesson_3.py
+++ lesson_3/lesson_3.py
@@ -1,13 +1,10 @@
-from collections import Counter
-
-goods = "папайя:14,яблоко:10,банан:5,яблоко:7,апельсин:3,папайя:88"
+goods = "папайя:14,яблоко:10,банан:10,яблоко:27,апельсин:3,папайя:88"
 goods_list = goods.split(",")
 goods_list = [good.split(":") for good in goods_list]
+#goods_dict = {2:3}
+goods_dict = {}
 
-goods_dict = Counter()
-for key, value in goods_list:
-   goods_dict[key] += int(value)
-
-dict(goods_dict)
+for k , value in goods_list:
+  goods_dict[k] = goods_dict.get(k, 0) + int(value)
 
 print(goods_dict)
